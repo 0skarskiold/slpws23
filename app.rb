e0935588ef1295ec6f49861323c2f0149fa8e304
0skarskiold <oskarskioldlundquist@gmail.com>
2023-02-07 11:19:46 +0100

    gjorde lite av en ui och fixade så att varor visas på starskärmen

--- app.rb
+++ app.rb
@@ -3,16 +3,16 @@ require 'slim'
 require 'sqlite3'
 
 
+
 enable :sessions
 
 
 get('/')  do
-    
-    db = SQLite3::Database.new("db/handlaonline.db")
-    db.results_as_hash = true
-    result = db.execute("SELECT pris FROM varor WHERE id=?", 1)
-    
-    p result
+    db = SQLite3::Database.new("data/handlaonline.db")
+    @result = db.execute("SELECT * FROM varor")
     slim(:start)
 end 
 
+get('/') do
+    
+end
