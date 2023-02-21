788235993b57369bf675121dc56461f6ac7fbd0f
0skarskiold <oskarskioldlundquist@gmail.com>
2023-02-21 11:53:09 +0100

    fixade login och signup

--- app.rb
+++ app.rb
@@ -1,9 +1,7 @@
 require 'sinatra'
 require 'slim'
 require 'sqlite3'
-
-
-
+require 'bcrypt'
 enable :sessions
 
 
@@ -13,6 +11,45 @@ get('/')  do
     slim(:start)
 end 
 
-get('/') do
-    
+get('/showsignup') do
+    slim(:signup)
+end
+
+post('/signup') do
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+    mail = params[:mail]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+  
+    if password == password_confirm
+      password_digest = BCrypt::Password.create(password)
+      db = SQLite3::Database.new('data/handlaonline.db')
+      db.execute("INSERT INTO users (förnamn,efternamn,mail,lösenord,is_admin) VALUES (?,?,?,?,?)",first_name,last_name,mail,password_digest,0)
+      redirect('/')
+    else
+      "Lösenorden matchade inte :("
+    end
+  end
+
+get('/showlogin') do
+    slim(:login)
+end
+
+post('/login') do
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+    password = params[:password]
+    db = SQLite3::Database.new('data/handlaonline.db')
+    db.results_as_hash = true 
+    result = db.execute("SELECT * FROM users WHERE förnamn = ? AND efternamn = ?", first_name, last_name).first
+    pwdigest = result["lösenord"]
+    if BCrypt::Password.new(pwdigest) == password
+        name = result["förnamn"]
+        session[:name] = name
+        redirect('/')
+    else
+      "Fel lösenord"
+    end
+  
 end
